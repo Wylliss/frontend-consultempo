3167725d423664c23b9f67c58e45d4566fbc1a6c
fbueno12 <felipebueno181@gmail.com>
2021-11-01 10:53:25 -0300

    arquivos para jsx e temperatura card

---
+++ src/components/BodyComponent/index.jsx
@@ -0,0 +1,16 @@
+import './style.css'
+import MyCard from '../CardComponent';
+const Body = () => {
+  return (
+    <div class="bg_image">
+      <div className="weather_temperature">
+        <h4>Temperatura</h4>
+        <p>24°C</p>
+      </div>
+
+      {/* <MyCard /> */}
+    </div>
+  );
+}
+
+export default Body
